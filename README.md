239848322facf8e37e9324b7a779e971388d53c7
Adrian Castillo <rodacato@gmail.com>
2015-04-04 10:32:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# jQuery imageTile
+
+### A jump-start for jQuery plugins development
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin massa augue, consectetur in mollis id, accumsan tincidunt odio. 
+
+Pellentesque porttitor lectus nec purus pharetra, id posuere felis semper. Aenean laoreet sit amet neque auctor ullamcorper.
+Maecenas luctus nisl non efficitur euismod. Vestibulum luctus ornare quam, et condimentum nisi porta ut. Nullam sed porta mauris.
+Fusce dapibus malesuada consequat. Nam sodales gravida elit eget hendrerit. Quisque vel lacinia quam. Mauris consequat, felis eu consequat 
+vehicula, risus nulla euismod leo, et scelerisque turpis est vitae diam.
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	```
+
+2. Include plugin's code:
+
+	```html
+	<script src="dist/jquery.imageTile.min.js"></script>
+	```
+
+3. Call the plugin:
+
+	```javascript
+	$("#element").imageTile();
+	```
+
+## Guides
+
+#### How did we get here?
+
+Cras erat justo, scelerisque sit amet elit vel, venenatis maximus nisl. Donec aliquet, ligula sed viverra tempus, dolor mi sagittis velit, varius semper diam enim eget risus. Ut ex eros, 
+laoreet a tempus sed, faucibus ultrices massa. Maecenas luctus, mi et cursus iaculis, felis nulla aliquam diam, sit amet dictum elit est sed nunc. Nunc sed magna mollis lorem pulvinar aliquam ut id ante.
+
+Fusce nec cursus velit. Quisque vulputate ipsum sit amet libero aliquet varius. Donec ut ipsum justo. Nulla mollis enim augue, luctus gravida odio dignissim eget. Sed at fermentum nisi. 
+Cras vitae elementum sapien. Fusce in mauris id eros faucibus finibus. Phasellus volutpat vestibulum euismod.
+
+## Contributing
+
+Check [CONTRIBUTING.md](https://github.com/rodacato/jquery-image-tile/blob/master/CONTRIBUTING.md) for more information.
+
+## History
+
+Check [Releases](https://github.com/rodacato/jquery-image-tile/releases) for detailed changelog.
+
+## License
+
+[MIT License](http://rodacato.mit-license.org/) © Adrian Castillo
---
+++ src/jquery.imageTile.js
@@ -0,0 +1,157 @@
+/*
+ *  Project: ImageTile
+ *  Description: Convert a set of images on tiles.
+ *  Author: Adrian Castillo
+ *  License: MIT
+ */
+
+;(function ($, window, document, undefined) {
+
+    var pluginName = "imageTile",
+        dataKey = "plugin_" + pluginName;
+
+    // Private Methods
+    var setMetadata = function(ele) {
+      var parent = $(ele).parent('li'),
+          data = {
+            loaded: true,
+            full: false,
+            initialized: false,
+            index: parent.index(),
+            initialIndex: parent.data('image-index') || 0,
+            height: ele.height,
+            originalImageSrc: $(ele).attr('src'),
+            tileHeight: ele.height / this.options.verticalTiles,
+            tileWidth: ele.width / this.options.horizontalTiles,
+            width: ele.width,
+          };
+      return parent.data(data);
+    };
+
+    var updateProperties = function(ele, img, data){
+      var parent = $(ele).parent('li');
+
+      // Show image index desired
+      var index =  data.index;
+      if ( !data.initialized ){
+        index = data.initialIndex;
+      }
+
+      x = parseInt(index % this.options.horizontalTiles);
+      y = parseInt(index / this.options.horizontalTiles);
+
+      properties = {
+        'background-image': 'url(' + img + ')',
+        'background-position-x': '-' + (x * data.tileWidth) + 'px',
+        'background-position-y': '-' + (y * data.tileHeight) + 'px',
+        'height': data.tileHeight + 'px',
+        'width': data.tileWidth + 'px'
+      };
+      parent.css(properties);
+
+      // Set copy image placeholder
+      if ( !parent.data('initialized') ){
+        parent.find('.placeholder.image').css(properties);
+      }
+
+      parent.data('initialized', true);
+    };
+
+    var bindItemClick = function(ele){
+      var that = this;
+      $(ele).parent('li').on('click', function(){
+        var target = $(this),
+            outerImage = target.find('img');
+
+        if ( target.data().full ){
+          target.data('full', false).removeClass('image-active');
+          restoreTiles(that);
+        } else {
+          target.data('full', true).addClass('image-active');
+          changeTiles(that, outerImage.attr('src'));
+        }
+      });
+    };
+
+    var changeTiles = function(scope, src){
+      scope.element.find('li').each(function(index, item){
+        var  innerImage = $(item).find('img');
+        updateProperties.call(scope, innerImage, src, $(item).data());
+      }).addClass('is-full-image');
+    };
+
+    var restoreTiles = function(scope){
+      scope.element.find('li').each(function(index, item){
+        var innerImage = $(item).find('img')
+        $(item).data('initialized', false);
+        updateProperties.call(scope, innerImage, innerImage.attr('src'), $(item).data());
+      }).removeClass('is-full-image');
+    };
+
+
+
+
+
+
+    var ImageTile = function (element, options) {
+        this.element = element;
+
+        this.options = {
+          textColor: "#000",
+          horizontalTiles: 4,
+          verticalTiles: 2,
+        };
+
+        /*
+         * Initialization
+         */
+
+        this.init(options);
+    };
+
+    ImageTile.prototype = {
+        // initialize options
+        init: function (options) {
+            $.extend(this.options, options);
+
+            var that = this;
+
+            // Hide images, and bind load event to get width/height
+            this.element.find('li > img').load(function(){
+              var image = $(this),
+                  parent = image.parent('li');
+              setMetadata.call(that, this)
+              updateProperties.call(that, image, this.src, parent.data());
+              bindItemClick.call(that, this);
+            }).data({ loaded: false })
+        },
+
+        // Public methods
+        publicMethod: function () {
+
+        }
+    };
+
+    /*
+     * Plugin wrapper, preventing against multiple instantiations and
+     * return plugin instance.
+     */
+    $.fn[pluginName] = function (options) {
+
+        var plugin = this.data(dataKey);
+
+        // has plugin instantiated ?
+        if (plugin instanceof ImageTile) {
+            // if have options arguments, call plugin.init() again
+            if (typeof options !== 'undefined') {
+                plugin.init(options);
+            }
+        } else {
+            plugin = new ImageTile(this, options);
+            this.data(dataKey, plugin);
+        }
+
+        return plugin;
+    };
+
+}(jQuery, window, document));
